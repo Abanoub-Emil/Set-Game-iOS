51396a33ce40bbabc62b79ba284e3022ccab0607
Abanoub <Abanoub@macossierra-hagar.local>
2018-08-19 12:19:40 +0200

    Added deal 3 more cards button and Made some Ui enhancements

---
+++ SetCards/Model/PlayingCards.swift
@@ -0,0 +1,25 @@
+//
+//  PlayingCards.swift
+//  SetCards
+//
+//  Created by Abanoub on 8/19/18.
+//  Copyright © 2018 Abanoub. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct PlayingCards {
+    
+    var allImagesNames = ["CylinderG1", "CylinderG2", "CylinderG3",
+                          "CylinderR1", "CylinderR2", "CylinderR3",
+                          "CylinderP1", "CylinderP2", "CylinderP3",
+                          "SG1", "SG2", "SG3",
+                          "SR1", "SR2", "SR3",
+                          "SP1", "SP2", "SP3",
+                          "DiamondG1", "DiamondG2", "DiamondG3",
+                          "DiamondR1", "DiamondR2", "DiamondR3"]
+    
+    
+    
+}
