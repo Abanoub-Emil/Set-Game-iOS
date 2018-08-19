5a7b9443f42acae8d47136295b4bdd441ab5e932
Abanoub <Abanoub@macossierra-hagar.local>
2018-08-19 10:04:54 +0200

    Put all images

---
+++ SetCards/ViewControllers/SetGameViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SetGameViewController.swift
+//  SetCards
+//
+//  Created by Abanoub on 8/19/18.
+//  Copyright © 2018 Abanoub. All rights reserved.
+//
+
+import UIKit
+
+class SetGameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+  
+
+}
+
---
+++ SetCards/ViewModels/SetGameViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  SetGameViewModel.swift
+//  SetCards
+//
+//  Created by Abanoub on 8/19/18.
+//  Copyright © 2018 Abanoub. All rights reserved.
+//
+
+import Foundation
+
+class SetGameViewModel {
+    
+    
+    
+}
